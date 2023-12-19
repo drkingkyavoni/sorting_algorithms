8b7bd03aae5d26b8c2a7626998d6ff56d46f12e1
Kyuka Ibrahim Avong <drkyavoni@live.com>
2023-12-19 13:14:47 +0100

    "v0.1.1"

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stddef.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -10,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /**
