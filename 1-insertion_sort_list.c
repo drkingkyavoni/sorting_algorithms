90a14e42e44699f0f8f11f0f6f99844123b06735
Kyuka Ibrahim Avong <drkyavoni@live.com>
2023-12-23 21:28:55 +0100

    "v0.1.5.1"

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,4 @@
 #include "sort.h"
-
 /**
  * insertion_sort_list - function that sorts a doubly
  * linked list of integers in ascending order
@@ -8,10 +7,34 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *sorted, *temp;
+    listint_t *current, *next_node;
 
-	/* if list is not null nor (*list)->next not null */
-		/* count the number of nodes */
-		/* loop through the list */
+    if (!list || !(*list) || !(*list)->next)
+        return;
 
+    current = (*list)->next;
+
+    while (current)
+    {
+        next_node = current->next;
+
+        while (current->prev && current->n < current->prev->n)
+        {
+            current->prev->next = current->next;
+            if (current->next)
+                current->next->prev = current->prev;
+            current->next = current->prev;
+            current->prev = current->next->prev;
+            current->next->prev = current;
+
+            if (current->prev)
+                current->prev->next = current;
+            else
+                *list = current;
+
+            print_list(*list);
+        }
+
+        current = next_node;
+    }
 }
